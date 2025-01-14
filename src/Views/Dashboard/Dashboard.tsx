9a213de08a02541cc8537aaad2bceb8f7ad1b306
Oleksii <oleksprokopchenko@gmail.com>
2025-01-14 23:31:37 +0200

    feature: implemented AgeWidget component with hardcoded data

--- src/Views/Dashboard/Dashboard.tsx
+++ src/Views/Dashboard/Dashboard.tsx
@@ -1,6 +1,7 @@
 import NavBar from "@/Features/NavBar/Navbar";
 import styles from "./Dashboard.module.scss";
 import { TrackerWidget } from "@/Features/TrackerWidget/TrackerWidget";
+import { AgeWidget } from "@/Features/AgeWidget/AgeWidget";
 
 const Dashboard = () => {
 	return (
@@ -10,6 +11,7 @@ const Dashboard = () => {
 				<div className={styles["Dashboard-model"]}></div>
 				<div className={styles["Dashboard-stats"]}>
 					<TrackerWidget />
+					<AgeWidget />
 				</div>
 			</div>
 		</div>

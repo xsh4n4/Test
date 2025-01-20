00ac15e6dc6adab6dc1e189363e964a4ee908919
Jian Zhu <jianzhu0119@gmail.com>
2025-01-20 06:58:12 -0800

    fix: update the upload files component

--- src/App/Consts/index.ts
+++ src/App/Consts/index.ts
@@ -4,3 +4,22 @@ export const ConfigControlItems = {
 	UploadFiles: "Upload Files",
 	ConnectApp: "Connect App / Device",
 };
+
+export const UploadFileTypes = {
+	Microbiome: {
+		title: "Microbiome",
+		fileTypes: [".fastq"],
+		fileSize: 25,
+	},
+	Blood: {
+		title: "Blood",
+		fileTypes: [".pdf"],
+		extra: "PDF",
+		fileSize: 25,
+	},
+	Genetic: {
+		title: "Genetic",
+		fileTypes: [".fastq", ".vcf"],
+		fileSize: 25,
+	},
+};

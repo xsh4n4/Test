22e7104b49a1e22e7c7ac8b7e285971f37fb72f2
Jian Zhu <jianzhu0119@gmail.com>
2025-01-22 02:40:30 -0800

    fix: update the login page

--- src/Features/Auth/Login/Components/LoginForm/LoginForm.tsx
+++ src/Features/Auth/Login/Components/LoginForm/LoginForm.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import styles from "./LoginForm.module.scss";
 import PasswordHidden from "@assets/Auth/PasswordHidden.svg?react";
-import CheckMark from "@assets/Auth/CheckMark.svg?react";
+import CheckMarkFilledIcon from "@assets/General/CheckMarkFilled.svg?react";
 import { useNavigate } from "react-router-dom";
 import { paths } from "@/App/Routes/Paths";
 import { toast } from "react-toastify";
@@ -28,43 +28,56 @@ export const LoginForm = () => {
 
 	return (
 		<div className={styles["Auth-content-form"]}>
-			<div className={styles["Auth-content-form-title"]}>
-				<h2>Sign In</h2>
-				<h4>Sign in if you already have an account.</h4>
-			</div>
-
-			<div className={styles["Auth-content-form-inputs"]}>
-				<input
-					type='email'
-					placeholder='Email address'
-					value={email}
-					onChange={(e) => setEmail(e.target.value)}
-				/>
-
-				<div className={styles["Auth-content-form-inputs-password-wrapper"]}>
-					<input
-						type={hidePassword ? "password" : "text"}
-						placeholder='Password'
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-					/>
-					<div className={styles["Auth-content-form-inputs-password-hidden"]}>
-						<PasswordHidden onClick={() => setHidePassword(!hidePassword)} />
+			<div className={styles["info"]}>
+				<div className={styles["title-description"]}>
+					<div className={styles["title"]}>Sign In</div>
+					<div className={styles["description"]}>
+						Sign in if you already have an account.
+					</div>
+				</div>
+				<div className={styles["inputs-wrapper"]}>
+					<div className={styles["email"]}>
+						<label htmlFor='email'>Email address</label>
+						<input
+							type='email'
+							placeholder='Email address'
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+						/>
+					</div>
+					<div className={styles["password"]}>
+						<label htmlFor='password'>Password</label>
+						<div className={styles["password-wrapper"]}>
+							<input
+								type={hidePassword ? "password" : "text"}
+								placeholder='Password'
+								value={password}
+								onChange={(e) => setPassword(e.target.value)}
+							/>
+							<div className={styles["password-hidden"]}>
+								<PasswordHidden
+									onClick={() => setHidePassword(!hidePassword)}
+								/>
+							</div>
+						</div>
+					</div>
+					<div className={styles["forgot-password"]}>
+						<a href='#'>Forgot password?</a>
 					</div>
 				</div>
 			</div>
-			<div className={styles["Auth-content-form-forgot-password"]}>
-				<a href='#'>Forgot password?</a>
-			</div>
 
-			<button
-				className={styles["Auth-content-form-signin-button"]}
-				onClick={handleLogin}
-			>
-				Sign In <CheckMark />
-			</button>
-			<div className={styles["Auth-content-form-signup"]}>
-				Don’t have an account? <a href='/register'>Sign Up</a>
+			<div className={styles["actions"]}>
+				<div className={styles["signup"]}>
+					Don’t have an account? <a href='/register'>Sign Up</a>
+				</div>
+				<button
+					className={styles["signin"]}
+					disabled={!email && !password}
+					onClick={handleLogin}
+				>
+					Sign In <CheckMarkFilledIcon />
+				</button>
 			</div>
 		</div>
 	);

56518d492e565db94322189c30429774fbbca1d3
Jian Zhu <jianzhu0119@gmail.com>
2025-01-23 09:27:00 -0800

    fix: update the risk detail page

--- src/Views/DetailedRisk/DetailedRisk.tsx
+++ src/Views/DetailedRisk/DetailedRisk.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import NavBar from "@/Features/Structural/NavBar/Navbar";
 import styles from "./DetailedRisk.module.scss";
@@ -9,6 +10,7 @@ import { detailedSystemConcerns } from "@/Features/Dashboard/ConcernsWidget/help
 import { PlanWidget } from "@/Features/Risk/PlanWidget/PlanWidget";
 import GoalsProgressMenu from "@/Features/Risk/GoalProgressMenu/GoalProgressMenu";
 import { ReasonsTable } from "@/Features/Risk/ReasonsTable/ReasonsTable";
+import Logo from "@assets/General/IconGenetiq.svg?react";
 
 function toTitleCase(str: string): string {
 	return str
@@ -21,8 +23,15 @@ function toTitleCase(str: string): string {
 
 const DetailedRisk = () => {
 	const { riskName } = useParams();
+	const [isLoading, setIsLoading] = useState<boolean>(true);
 
 	const formattedName = riskName ? toTitleCase(riskName) : "";
+
+	const handleIframeLoad = () => {
+		setInterval(() => {
+			setIsLoading(false);
+		}, 4000);
+	};
 
 	return (
 		<div className={styles["DetailerRisk-layout"]}>
@@ -30,7 +39,9 @@ const DetailedRisk = () => {
 
 			<CameraProvider>
 				<div className={styles["DetailerRisk-content"]}>
-					<div className={styles["DetailerRisk-stats"]}>
+					<div
+						className={`${styles["DetailerRisk-stats"]} ${styles["DetailerRisk-animate"]}`}
+					>
 						<RiskHeader title={formattedName} />
 						<RiskStatus />
 						<div className={styles["DetailerRisk-age"]}>
@@ -57,14 +68,28 @@ const DetailedRisk = () => {
 					</div>
 					<div className={styles["DetailerRisk-twin"]}>
 						<GoalsProgressMenu />
-						<iframe
-							id='embedded-human'
-							frameBorder='0'
-							allowFullScreen
-							style={{ aspectRatio: "4 / 3", width: "100%" }}
-							loading='lazy'
-							src='https://human.biodigital.com/viewer/?id=5vyL&ui-anatomy-descriptions=true&ui-anatomy-pronunciations=true&ui-anatomy-labels=false&ui-audio=true&ui-chapter-list=false&ui-fullscreen=false&ui-help=false&ui-info=false&ui-label-list=true&ui-layers=false&ui-skin-layers=false&ui-loader=circle&ui-media-controls=none&ui-menu=false&ui-nav=false&ui-search=false&ui-tools=false&ui-tutorial=false&ui-undo=false&ui-whiteboard=false&initial.none=true&disable-scroll=false&dk=57a9053995a029ade6a11d83c8a64a4fedef2b19&paid=o_27f525a0'
-						></iframe>
+						<div
+							style={{
+								position: "relative",
+								width: "100%",
+								aspectRatio: "4 / 3",
+							}}
+						>
+							{isLoading && (
+								<div className={styles["DetailerRisk-iframe-loading"]}>
+									<Logo className={styles["DetailerRisk-iframe-logo"]} />
+								</div>
+							)}
+							<iframe
+								id='embedded-human'
+								frameBorder='0'
+								allowFullScreen
+								style={{ aspectRatio: "4 / 3", width: "100%" }}
+								loading='lazy'
+								onLoad={handleIframeLoad}
+								src='https://human.biodigital.com/viewer/?id=5vyL&ui-anatomy-descriptions=true&ui-anatomy-pronunciations=true&ui-anatomy-labels=false&ui-audio=true&ui-chapter-list=false&ui-fullscreen=false&ui-help=false&ui-info=false&ui-label-list=true&ui-layers=false&ui-skin-layers=false&ui-loader=circle&ui-media-controls=none&ui-menu=false&ui-nav=false&ui-search=false&ui-tools=false&ui-tutorial=false&ui-undo=false&ui-whiteboard=false&initial.none=true&disable-scroll=false&dk=57a9053995a029ade6a11d83c8a64a4fedef2b19&paid=o_27f525a0'
+							></iframe>
+						</div>
 					</div>
 				</div>
 			</CameraProvider>

4f3a52f535070228bdc63d51a3705120e43aadd2
Oleksii <oleksprokopchenko@gmail.com>
2025-01-23 18:05:03 +0200

    feat: added hardcoded titles and descs to Widgets on SystemOverview page

--- src/Views/SystemOverview/SystemOverview.tsx
+++ src/Views/SystemOverview/SystemOverview.tsx
@@ -7,19 +7,87 @@ import { CameraProvider } from "@/Features/DigitalTwin/Context/CameraContext";
 // import CtaModal from "@/Features/Dashboard/CtaModal/CtaModal";
 import { ConcernsWidget } from "@/Features/Dashboard/ConcernsWidget/ConcernsWidget";
 import { Link } from "react-router-dom";
+import { ReasonsTable } from "@/Features/Dashboard/ConcernsWidget/Components/ReasonsTable/ReasonsTable";
+import { detailedSystemConcerns } from "@/Features/Dashboard/ConcernsWidget/helpers/detailedSystemConcerns";
+import { PlanWidget } from "@/Features/Dashboard/PlanWidget/PlanWidget";
 
 const SystemOverview = () => {
 	const { systemName } = useParams();
+
 	return (
 		<div className={styles["SystemOverview-layout"]}>
 			<NavBar />
 			<CameraProvider>
 				<div className={styles["SystemOverview-content"]}>
 					<div className={styles["SystemOverview-stats"]}>
-						<Link to='/dashboard'>Back Home</Link>
-						<h1 className={styles["SystemOverview-title"]}>{systemName}</h1>
-						<AgeWidget />
-						<ConcernsWidget category={"total"} />
+						<div className={styles["SystemOverview-head"]}>
+							<Link to='/dashboard'>Back Home</Link>
+							<h1 className={styles["SystemOverview-title"]}>{systemName}</h1>
+						</div>
+						<div className={styles["SystemOverview-widget-wrapper"]}>
+							<div className={styles["SystemOverview-widget-head"]}>
+								<h4 className={styles["SystemOverview-widget-title"]}>
+									What your age tells about your health
+								</h4>
+								<p className={styles["SystemOverview-widget-desc"]}>
+									A younger cardiovascular age can indicate better heart health
+									and potentially lower your risk of complications from AFib,
+									making it essential to focus on heart health measures tailored
+									to your condition.
+								</p>
+							</div>
+							<AgeWidget />
+						</div>
+						<div className={styles["SystemOverview-widget-wrapper"]}>
+							<div className={styles["SystemOverview-widget-head"]}>
+								<h4 className={styles["SystemOverview-widget-title"]}>
+									Key areas of concern
+								</h4>
+								<p className={styles["SystemOverview-widget-desc"]}>
+									Based on the provided data and individual disease
+									recommendations, the patient is at risk for several
+									cardiovascular conditions, including:
+								</p>
+							</div>
+							<ConcernsWidget category={"total"} />
+						</div>
+						<div className={styles["SystemOverview-widget-wrapper"]}>
+							<div className={styles["SystemOverview-widget-head"]}>
+								<h4 className={styles["SystemOverview-widget-title"]}>
+									How we known this
+								</h4>
+								<p className={styles["SystemOverview-widget-desc"]}>
+									The primary concerns are elevated LDL cholesterol levels,
+									slightly below optimal HDL cholesterol, and the need for
+									improved cardiovascular fitness and stress management.
+								</p>
+							</div>
+							<ReasonsTable
+								reasons={detailedSystemConcerns[0].details[0].reasons}
+								detailIndex={1}
+							/>
+						</div>
+						<div className={styles["SystemOverview-widget-wrapper"]}>
+							<div className={styles["SystemOverview-widget-head"]}>
+								<h4 className={styles["SystemOverview-widget-title"]}>
+									What you can do
+								</h4>
+								<p className={styles["SystemOverview-widget-desc"]}>
+									The following comprehensive action plan combines lifestyle
+									changes, monitoring strategies, and supplement recommendations
+									to support overall cardiovascular health.
+								</p>
+								<p className={styles["SystemOverview-widget-desc"]}>
+									By following this comprehensive action plan, you can
+									effectively manage cardiovascular risk factors, improve heart
+									health, and reduce the likelihood of disease progression.
+									Regular monitoring and lifestyle adjustments will play a
+									crucial role in maintaining optimal cardiovascular health and
+									overall well-being.
+								</p>
+							</div>
+							<PlanWidget />
+						</div>
 					</div>
 					<div className={styles["SystemOverview-dt-container"]}>
 						<div className={styles["SystemOverview-model"]}>
